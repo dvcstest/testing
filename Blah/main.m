38ad5cac84925fb3e451031b043bd4a505710610
Kieran Senior <kieran_senior@apple.com>
2018-01-23 12:38:37 -0800

    Initial Commit

---
+++ Blah/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Blah
+//
+//  Created by Kieran Senior on 1/23/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
